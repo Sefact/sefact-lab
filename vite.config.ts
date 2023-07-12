9d0bf83e72e207451c1626cee781be39574d1aab
Sefact <lime17442@gmail.com>
2023-07-12 22:59:16 +0900

    vite config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: './',
 	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@src': path.resolve(__dirname, './src'),
+			'@api': path.resolve(__dirname, './src/api'),
+		},
+	},
 });
